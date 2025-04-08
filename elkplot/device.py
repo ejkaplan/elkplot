e5e703f41e0858d830a9cc041091dcef94be2124
El Kaplan <3373930+ejkaplan@users.noreply.github.com>
2025-04-08 10:31:16 -0400

    update default pen up down positions

--- elkplot/device.py
+++ elkplot/device.py
@@ -75,8 +75,8 @@ def plan_layer_proc(
 class Device:
     def __init__(
         self,
-        pen_up_position: float = 0,
-        pen_down_position: float = -150,
+        pen_up_position: float = 20,
+        pen_down_position: float = -50,
         pen_up_speed: float = 150,
         pen_down_speed: float = 150,
         pen_up_delay: int = 50,
@@ -227,7 +227,7 @@ class Device:
     def read_position(self) -> tuple[float, float]:
         """Get the xy coordinates of the pen"""
         response = self._command("QS")
-        self._readline() # Clear out the 'OK'
+        self._readline()  # Clear out the 'OK'
         a, b = map(float, response.split(","))
         a /= self.steps_per_unit
         b /= self.steps_per_unit
@@ -302,7 +302,7 @@ class Device:
     def pen_down(self):
         """Lower the pen"""
         return self._command("SP", 0, self.pen_down_delay, self.pen_lift_pin)
-    
+
     def powered_on(self):
         stepper, motor = (int(elem) for elem in self._command("QC").split(","))
         self._readline()
